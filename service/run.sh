8a8b2c585798024f18c4cf1b4eff20ff4da3c47a
vanniuner <at18708169062@gmail.com>
2023-03-22 14:41:56 +0800

    add: controller demo

---
+++ service/run.sh
@@ -0,0 +1,15 @@
+DEBUG_PORT=$1
+MAVEN_RELOAD=$2
+mvn clean -T 1C package -f ../pom.xml $MAVEN_RELOAD -Dmaven.test.skip=true
+if [ -n $JREBEL_HOME ]; then
+    export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$DEBUG_PORT"
+    export JAVA_OPTS="$JAVA_OPTS -noverify -agentpath:$JREBEL_HOME/lib/libjrebel64.dylib"
+    echo "SETTING JREBEL=$JREBEL_HOME/lib/libjrebel64.dylib"
+fi
+java $JAVA_OPTS \
+-Xms1024m -Xmx1024m \
+-Dspring.application.name=your_app_name \
+-Dspring.cloud.nacos.config.file-extension=yml \
+-Dlogging.config=/var/pb/logback-spring.xml \
+-Duser.timezone=GMT+08 \
+-jar ./target/service-0.0.1-SNAPSHOT.jar
---
+++ service/src/main/java/com/land/template/controller/DemoController.java
@@ -0,0 +1,30 @@
+package com.land.template.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author LZJ
+ * @time 2023-03-22 14:35:46
+ **/
+@Data
+@Slf4j
+@RestController
+@NoArgsConstructor
+public class DemoController {
+
+    /**
+     * 健康检查
+     */
+    @RequestMapping("/healthCheck")
+    public ResponseEntity<String> welcome() {
+        log.info("==== [log]: {}");
+        return ResponseEntity.ok("pong");
+    }
+
+}
